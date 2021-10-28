f8dc71783f9cd2247d311eb0d2708a5fc2ca5d86
GMR Student <gmrstudent@outlook.com>
2021-10-27 18:27:35 -0700

    Finish the graphing and regression code

---
+++ Line of Best Fit Project/Line of Best Fit Project/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Line_of_Best_Fit_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.xValue = new System.Windows.Forms.NumericUpDown();
+            this.yValue = new System.Windows.Forms.NumericUpDown();
+            this.Add = new System.Windows.Forms.Button();
+            this.LinearRegression = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(668, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input a point:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "x:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(631, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "y:";
+            // 
+            // xValue
+            // 
+            this.xValue.Location = new System.Drawing.Point(652, 66);
+            this.xValue.Name = "xValue";
+            this.xValue.Size = new System.Drawing.Size(120, 20);
+            this.xValue.TabIndex = 3;
+            // 
+            // yValue
+            // 
+            this.yValue.Location = new System.Drawing.Point(652, 92);
+            this.yValue.Name = "yValue";
+            this.yValue.Size = new System.Drawing.Size(120, 20);
+            this.yValue.TabIndex = 4;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(634, 118);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(154, 57);
+            this.Add.TabIndex = 5;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // LinearRegression
+            // 
+            this.LinearRegression.Location = new System.Drawing.Point(652, 200);
+            this.LinearRegression.Name = "LinearRegression";
+            this.LinearRegression.Size = new System.Drawing.Size(120, 179);
+            this.LinearRegression.TabIndex = 6;
+            this.LinearRegression.Text = "Regress";
+            this.LinearRegression.UseVisualStyleBackColor = true;
+            this.LinearRegression.Click += new System.EventHandler(this.LinearRegression_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LinearRegression);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.yValue);
+            this.Controls.Add(this.xValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.xValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown xValue;
+        private System.Windows.Forms.NumericUpDown yValue;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button LinearRegression;
+    }
+}
+
---
+++ Line of Best Fit Project/Line of Best Fit Project/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Line_of_Best_Fit_Project
+{
+    public partial class Form1 : Form
+    {
+        public static List<(decimal, decimal)> dataPoints;
+        public static decimal M;
+        public static decimal B;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataPoints = new List<(decimal, decimal)>();
+            M = 0;
+            B = 0;
+        }
+
+        public static decimal LinearEquationSolver(decimal x)
+        {
+            return (x * M) + B;
+        }
+
+        public static void Mutation()
+        {
+            Random rand = new Random();
+            int variable = rand.Next(2);
+            if(variable == 0)
+            {
+                int increment = rand.Next(2);
+                if(increment == 0)
+                {
+                    M+=0.1m; 
+                }
+                else
+                {
+                    M -= 0.1m;
+                }
+            }
+            else
+            {
+                int increment = rand.Next(2);
+                if (increment == 0)
+                {
+                    B += 0.1m;
+                }
+                else
+                {
+                    B -= 0.1m;
+                }
+            }
+        }
+
+        public static decimal MeanAbsoluteError()
+        {
+            decimal summation = 0;
+            foreach (var point in dataPoints)
+            {
+                summation += Math.Abs(LinearEquationSolver(point.Item1) - point.Item2);
+            }
+            return summation / dataPoints.Count();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            dataPoints.Add((xValue.Value, yValue.Value));
+
+        }
+
+        private void LinearRegression_Click(object sender, EventArgs e)
+        {
+            //Add the linear regression stuff.
+        }
+
+
+        //Add the graph functionality
+    }
+}
